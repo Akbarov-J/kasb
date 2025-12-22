3a31cb2ad75cf2039707616bcc234843881b64b4
Xusniddin Qalandarov <163927213+Xusniddin-devv@users.noreply.github.com>
2025-12-22 18:30:21 +0500

    Add select button for document type filtering and update button labels to 'Download'

--- src/app/features/client/main/components/documents/documents.ts
+++ src/app/features/client/main/components/documents/documents.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectButton } from 'primeng/selectbutton';
 
 @Component({
   selector: 'app-documents',
-  imports: [],
+  imports: [FormsModule, SelectButton],
   templateUrl: './documents.html',
   styleUrl: './documents.scss',
 })
 export class Documents {
+  selectedType = signal<number>(1);
 
+  typeOptions = signal<any>([
+    { label: 'Dasturlar', value: 1 },
+    { label: 'Qarorlar', value: 2 },
+  ]);
 }
